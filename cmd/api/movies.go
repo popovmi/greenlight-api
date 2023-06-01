126df34b3f1f6d3e1c0e8438dee8c889505e17dc
aenkas <mi69popov@gmail.com>
2023-06-02 02:56:58 +0300

    unmarshal runtime

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -9,10 +9,10 @@ import (
 
 func (self *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
 	var input struct {
-		Title   string   `json:"title"`
-		Year    int32    `json:"year"`
-		Runtime int32    `json:"runtime"`
-		Genres  []string `json:"genres"`
+		Title   string       `json:"title"`
+		Year    int32        `json:"year"`
+		Runtime data.Runtime `json:"runtime"`
+		Genres  []string     `json:"genres"`
 	}
 
 	err := self.readJSON(w, r, &input)
